cd856e8c20eaeb434c3990e7521ae73fbc4cd5c8
unknown <zia2054>
2023-05-28 16:53:40 +0900

    chore: 초기화

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -1,16 +1,11 @@
-import logging
-
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
 from django.shortcuts import render, get_object_or_404
 
 from ..models import Question
 
-logger = logging.getLogger('pybo')
-
 
 def index(request):
-    logger.info("INFO 레벨로 출력")
     """
     pybo 목록 출력
     """
