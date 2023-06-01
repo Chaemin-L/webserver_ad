abab7516ef9ccceb05f81b27da25c5d64e75f3ed
Chaemin-L <chaem12@kookmin.ac.kr>
2023-06-02 03:01:49 +0900

    docs: 불필요한 주석 삭제

--- mysite/pybo/views/base_views.py
+++ mysite/pybo/views/base_views.py
@@ -13,7 +13,6 @@ def index(request):
     page = request.GET.get('page', '1')  # 페이지
     kw = request.GET.get('kw', '')  # 검색어
     so = request.GET.get('so', 'recent')  # 정렬기준
-    #so_comment = request.GET.get('so_comment', 'recent')  # 정렬기준
 
     # 정렬
     if so == 'recommend':
